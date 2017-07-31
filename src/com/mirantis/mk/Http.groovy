699cc0ebc46d82e43a2c4bd1827d880cbe7b05a9
Oleg Grigorov <golantster@gmail.com>
2017-07-31 11:29:16 +0300

    bugfix

--- src/com/mirantis/mk/Http.groovy
+++ src/com/mirantis/mk/Http.groovy
@@ -95,7 +95,7 @@ def sendHttpRequest(url, method = 'GET', data = null, headers = [:], read_timeou
             println("[HTTP] Request URL: ${url}, method: ${method}, headers: ${headers}, content: ${dataStr}")
         }
 
-        def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: "$httpMethod", requestBody: "${dataStr}", url: "${master.url}${uri}", 
+        def response = httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: "${httpMethod}", requestBody: "${dataStr}", url: "${master.url}${uri}", 
                                     customHeaders: customHttpHeaders, timeout: requestTimeOut
     }
         
